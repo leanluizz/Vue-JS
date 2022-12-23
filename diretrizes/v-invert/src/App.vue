60f5efbc4c3f082f414ef1f80a2e4f98f77f1cfc
Leandro Luiz <luizzleandro827@gmail.com>
2022-12-22 22:37:03 -0300

    add diretrizes

---
+++ diretrizes/v-invert/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Nav from './components/Nav.vue'
+import './assets/css/menu.css'
+
+
+export default {
+  name:'App',
+  components:{
+    Nav,
+  },
+  class:{
+    menu:"menu"
+  }
+}
+</script>
+
+<template>
+  <div v-bind:class="{menu: true}">
+  <Nav title="Invert"/>
+</div>
+</template>
+
---
+++ diretrizes/v-invert/src/components/Nav.vue
@@ -0,0 +1,12 @@
+<script>
+export default{
+    name:'App',
+    props: {
+        title:String,
+        img:String
+    }
+}
+</script>
+<template>
+    <h1>{{ this.title }}</h1>
+</template>
