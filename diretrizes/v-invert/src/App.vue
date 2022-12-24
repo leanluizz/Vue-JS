a9c9c3dc510010b83fef8d1503369a00675cb6ad
Leandro Luiz <luizzleandro827@gmail.com>
2022-12-24 02:40:39 -0300

    add CSS files

--- diretrizes/v-invert/src/App.vue
+++ diretrizes/v-invert/src/App.vue
@@ -1,12 +1,13 @@
 <script>
 import Nav from './components/Nav.vue'
-import './assets/css/menu.css'
+import Main from './components/Main.vue'
 
 
 export default {
   name:'App',
   components:{
     Nav,
+    Main
   },
   class:{
     menu:"menu"
@@ -17,6 +18,9 @@ export default {
 <template>
   <div v-bind:class="{menu: true}">
   <Nav title="Invert"/>
+  <h1>Text</h1>
 </div>
+<Main />
+
 </template>
 
--- diretrizes/v-invert/src/components/Nav.vue
+++ diretrizes/v-invert/src/components/Nav.vue
@@ -1,12 +1,22 @@
 <script>
+import '../assets/css/menu.css'
 export default{
-    name:'App',
+    name:'app',
     props: {
-        title:String,
-        img:String
-    }
+        title:String
+    },
+    data(){
+        return{
+            RevertIcon:require("../assets/images/RevertIcon.gif"),
+            
+        }
+    },
+    class:{
+        widthIMG:"widthIMG"
+    },
 }
 </script>
 <template>
+    <img :src="RevertIcon" :class="{widthIMG:true}" alt="RevertIcon">
     <h1>{{ this.title }}</h1>
 </template>
