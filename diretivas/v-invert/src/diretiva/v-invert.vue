7963aee84649b1c896747eafa909532ea14d2ffe
Leandro Gomes <legomes@verointernet.com.br>
2022-12-26 18:06:41 -0300

    diretiva: v-invert add

---
+++ diretivas/v-invert/src/diretiva/v-invert.vue
@@ -0,0 +1,26 @@
+<script>
+export default{
+    name:"app",
+    data(){
+        return{
+            output:"",
+
+        }
+    },
+    directives:{
+        invert:{
+            updated(el, binding){
+               const split = binding.value.split('')
+               const reverse = split.reverse();
+
+               el.innerHTML = reverse.join('')
+            }
+        }
+    }
+    
+}
+</script>
+
+<template>
+    // Crie seu componente (Input) e atribua a diretiva v-invert 
+</template>
